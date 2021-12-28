d16594333c14c7bacff762c1f97028602b571a0b
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 08:37:06 +0100

    refact:crea funciones

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,19 +1,26 @@
 export function fizzBuzz(inicio:number, fin:number) {
     let numeros:string=inicio.toString();
     for(let i = inicio+1;i<=fin;i++){
-        if(i%3==0 && i%5==0  || (i.toString().includes('3') && (i.toString().includes('5')))){
+        if(isMultiple(i,3) && isMultiple(i,5)  || (containString(i,'3') && containString(i,'5'))){
             numeros+="\n"+"FizzBuzz";
         }
-        else if(i%3==0 || (i.toString()).includes('3')){
+        else if(isMultiple(i,3) || containString(i,'3')){
             numeros+="\n"+"Fizz";
         }
-        else if(i%5==0|| (i.toString()).includes('5')){
+        else if(isMultiple(i,5)|| containString(i,'5')){
             numeros+="\n"+"Buzz";
         }
         else{
             numeros+="\n"+i.toString();
         }
     }
-    console.log(numeros)
     return numeros;
+}
+
+function isMultiple (number:number, multiple:number) {
+	return number % multiple === 0;
+}
+
+function containString (numero:number, caracter:string) {
+	return numero.toString().includes(caracter);
 }
