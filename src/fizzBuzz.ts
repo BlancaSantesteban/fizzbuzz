7b0615f6a3800d8d2b45aaeffd88a73cd3fe037b
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 12:11:48 +0100

    refact:naming

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,22 +1,30 @@
-export function fizzBuzz(inicio:number, fin:number) {
+export function fizzBuzz(inicio:number, fin:number,fizzBuzz:boolean) {
     let output:string=inicio.toString();
-
     for(let i = inicio+1;i<=fin;i++){
-        
-        if(isMultiple(i,3) && isMultiple(i,5)  || (hasString(i,'3') && hasString(i,'5'))){
-            output+="\n"+"FizzBuzz";
-        }
-        else if(isMultiple(i,3) || hasString(i,'3')){
-            output+="\n"+"Fizz";
-        }
-        else if(isMultiple(i,5)|| hasString(i,'5')){
-            output+="\n"+"Buzz";
+        if(fizzBuzz){
+            output=getFizzBuzz(i,output);
         }
         else{
             output+="\n"+i.toString();
         }
     }
     return output;
+}
+function getFizzBuzz(i:number,result:string){
+   
+    if(isMultiple(i,3) && isMultiple(i,5)  || (hasString(i,'3') && hasString(i,'5'))){
+        result+="\n"+"FizzBuzz";
+    }
+    else if(isMultiple(i,3) || hasString(i,'3')){
+        result+="\n"+"Fizz";
+    }
+    else if(isMultiple(i,5)|| hasString(i,'5')){
+        result+="\n"+"Buzz";
+    }
+    else{
+        result+="\n"+i.toString();
+    }
+    return result;
 }
 
 function isMultiple (number:number, multiple:number) {
