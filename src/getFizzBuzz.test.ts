387277254f779674aca96a220b067bef06a87663
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 13:22:11 +0100

    feat: el valor 2 devuelve 2

--- src/getFizzBuzz.test.ts
+++ src/getFizzBuzz.test.ts
@@ -6,4 +6,10 @@ describe("Get FizzBuzz", () => {
 
     expect(result).toEqual("1");
   });
+
+  it("el valor 2 devuelve 2", () => {
+    const result = getFizzBuzz(2);
+
+    expect(result).toEqual("2");
+  });
 });
--- src/getFizzBuzz.ts
+++ src/getFizzBuzz.ts
@@ -1,3 +1,7 @@
 export function getFizzBuzz(valor: number) {
+  if (valor === 2) {
+    return "2";
+  }
+  
   return "1";
 }
