76c69e0703aea53e10532c0b61eaadd254f6ace6
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 08:29:21 +0100

    feat:contiene numero

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,18 +1,19 @@
 export function fizzBuzz(inicio:number, fin:number) {
     let numeros:string=inicio.toString();
     for(let i = inicio+1;i<=fin;i++){
-        if(i%3==0 && i%5==0){
+        if(i%3==0 && i%5==0  || (i.toString().includes('3') && (i.toString().includes('5')))){
             numeros+="\n"+"FizzBuzz";
         }
-        else if(i%3==0){
+        else if(i%3==0 || (i.toString()).includes('3')){
             numeros+="\n"+"Fizz";
         }
-        else if(i%5==0){
+        else if(i%5==0|| (i.toString()).includes('5')){
             numeros+="\n"+"Buzz";
         }
         else{
             numeros+="\n"+i.toString();
         }
     }
+    console.log(numeros)
     return numeros;
 }
