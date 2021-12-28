937d4f06bbb0e591a6f06013a285c7fbe77041ee
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 08:06:38 +0100

    feat:saca listado completo

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,7 +1,18 @@
 export function fizzBuzz(inicio:number, fin:number) {
     let numeros:string=inicio.toString();
     for(let i = inicio+1;i<=fin;i++){
-        numeros+="\n"+i.toString();
+        if(i%3==0 && i%5==0){
+            numeros+="\n"+"FizzBuzz";
+        }
+        else if(i%3==0){
+            numeros+="\n"+"Fizz";
+        }
+        else if(i%5==0){
+            numeros+="\n"+"Buzz";
+        }
+        else{
+            numeros+="\n"+i.toString();
+        }
     }
     return numeros;
 }
